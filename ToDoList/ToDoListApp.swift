3e736331b5264da9c5d282e60e30297ad9706f41
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-01 20:44:22 +0630

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Khun Kyaw Kham Hti on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
