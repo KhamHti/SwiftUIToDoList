ade3864801e344e1c0da5d35ce4c22cecf334fea
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-03 00:18:13 +0630

    added ToDoListItem and user models

---
+++ ToDoList/Models/ToDoListItem.swift
@@ -0,0 +1,9 @@
+//
+//  ToDoListItem.swift
+//  ToDoList
+//
+//  Created by Khun Kyaw Kham Hti on 01/05/2023.
+//
+
+import SwiftUI
+import Foundation
---
+++ ToDoList/Models/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  ToDoList
+//
+//  Created by Khun Kyaw Kham Hti on 01/05/2023.
+//
+
+import SwiftUI
+import Foundation
