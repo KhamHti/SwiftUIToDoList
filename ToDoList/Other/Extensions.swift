b9a5a9203b4077bf2c47efdd4b40800973e687af
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-07 00:51:29 +0630

    create extensions for encodable

---
+++ ToDoList/Other/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  ToDoList
+//
+//  Created by Khun Kyaw Kham Hti on 07/05/2023.
+//
+
+import Foundation
+
+extension Encodable {
+    func asDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self) else {
+            return [:]
+        }
+        do {
+            let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+            return json ?? [:]
+        } catch {
+            return [:]
+        }
+    }
+}
