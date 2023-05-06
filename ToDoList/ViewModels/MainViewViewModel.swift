5301d36ab64ce2e4f9655967431a47d19eefa25d
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-07 00:53:13 +0630

    update main view model

--- ToDoList/ViewModels/MainViewViewModel.swift
+++ ToDoList/ViewModels/MainViewViewModel.swift
@@ -5,5 +5,23 @@
 //  Created by Khun Kyaw Kham Hti on 01/05/2023.
 //
 
-import SwiftUI
+import FirebaseAuth
 import Foundation
+
+class MainViewViewModel: ObservableObject {
+    @Published var currentUserId: String = ""
+    
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init () {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+}
--- ToDoList/ViewModels/RegisterViewViewModel.swift
+++ ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -4,6 +4,59 @@
 //
 //  Created by Khun Kyaw Kham Hti on 01/05/2023.
 //
-
-import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
+
+class RegisterViewViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    
+    init() {}
+    
+    func register() {
+        guard validate() else {
+            return
+        }
+        // register user
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard let userId = result?.user.uid else {
+                return
+            }
+            
+            self?.insertUser(id: userId)
+        }
+    }
+    
+    private func insertUser(id: String){
+        let newUser = User(id: id,
+                           email: email,
+                           name: name,
+                           joined: Date().timeIntervalSince1970)
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(id)
+            .setData(newUser.asDictionary())
+    }
+    
+    private func validate() -> Bool{
+        guard !name.trimmingCharacters(in: .whitespaces).isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return false
+        }
+        
+        guard email.contains("@") && email.contains(".") else {
+            return false
+        }
+        
+        guard password.count >= 6 else {
+            return false
+        }
+        
+        return true
+    }
+}
