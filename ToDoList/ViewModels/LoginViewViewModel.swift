f34414d022af2f262ba3b024b7d598bbe7b6c85c
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-07 00:52:38 +0630

    update login viewmodel

--- ToDoList/ViewModels/LoginViewViewModel.swift
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -5,20 +5,41 @@
 //  Created by Khun Kyaw Kham Hti on 01/05/2023.
 //
 
+import FirebaseAuth
 import Foundation
 
 class LoginViewViewModel: ObservableObject {
     //
     @Published var email = ""
     @Published var password = ""
+    @Published var errorMessage = ""
     
     init() {}
     
-    func login( ) {
+    func login() {
+        guard validate() else {
+            return
+        }
         
+        // try to log in
+        Auth.auth().signIn(withEmail: email, password: password)
     }
     
-    func validate(){
+    private func validate() -> Bool {
+        errorMessage = ""
         
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessage = "Please fill in all fields."
+            return false
+        }
+        
+        // email@foo.com
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter vlaid email."
+            return false
+        }
+        print("click on login")
+        return true
     }
 }
