da7c49ff1f3fa3dfe794a637b7302fdcf4c687e8
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-14 23:18:45 +0630

    done check mark update

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -5,13 +5,26 @@
 //  Created by Khun Kyaw Kham Hti on 01/05/2023.
 //
 
-import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ProfileViewViewModel: ObservableObject {
     init(){}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+            db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
