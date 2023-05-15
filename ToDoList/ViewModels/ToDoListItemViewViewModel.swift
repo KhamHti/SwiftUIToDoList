f36f0698cad46d3940bce65ced46a2b258f71fb0
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-05-15 21:58:50 +0630

    profile view and log out button

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,11 +5,29 @@
 //  Created by Khun Kyaw Kham Hti on 01/05/2023.
 //
 
-import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation 
 
 // viewmodel for single list item view
 // single item list
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+            db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
+
 }
